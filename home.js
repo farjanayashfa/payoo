3e69e34a61648ccb01807060b15dfe7e93292813
Farjana <farjanayashfa9@gmail.com>
2025-11-14 18:33:36 +0600

    Updated code

--- home.js
+++ home.js
@@ -1,11 +1,13 @@
+const validpin = 1234;
+const validaccountNumber = 12345678910 ;
 document.getElementById("add-money-btn").addEventListener("click",function(e){
     e.preventDefault();
     //console.log("clickeddddddd");
     const bank = document.getElementById("bank").value;
-    const accountNumber = document.getElementById("account-number").value;
+    const accountNumber = parseInt(document.getElementById("account-number").value);
     //const amount = document.getElementById("amount").value;
     const amountMoney = parseInt (document.getElementById("amount").value);
-    const pinNumber = document.getElementById("pin-number").value;
+    const pinNumber = parseInt(document.getElementById("pin-number").value);
    console.log(bank,accountNumber,amountMoney,pinNumber);
 
    //if i want to add reset field 
@@ -15,6 +17,14 @@ document.getElementById("add-money-btn").addEventListener("click",function(e){
 //document.getElementById("pin-number").value = "";
 const availableBalance = parseInt(document.getElementById("available-balance").innerText)
 console.log(availableBalance)
+if(pinNumber!== validpin){
+    alert("invalid pin");
+    return;
+}
+if(validaccountNumber!== accountNumber){
+   alert("invalid account number");
+   return;
+}
 const newAvailableBalance =amountMoney + availableBalance;
 document.getElementById("available-balance").innerText= newAvailableBalance;
 })
